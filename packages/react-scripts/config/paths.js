f9f7fcd7ae733303bc9d245588d8747a1cfc4eee
zhaojun <zhaojun@4paradigm.com>
2020-06-03 19:02:07 +0800

    webpack build目录变更为dist

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -83,7 +83,7 @@ const resolveOwn = relativePath => path.resolve(__dirname, '..', relativePath);
 module.exports = {
     dotenv: resolveApp('.env'),
     appPath: resolveApp('.'),
-    appBuild: resolveApp('build'),
+    appBuild: resolveApp('dist'),
     appPublic: resolveApp('public'),
     appHtml: resolveApp('public/index.html'),
     appIndexJs: resolveModule(resolveApp, 'src/index'),
@@ -118,7 +118,7 @@ if (
     module.exports = {
         dotenv: resolveOwn(`${templatePath}/.env`),
         appPath: resolveApp('.'),
-        appBuild: resolveOwn('../../build'),
+        appBuild: resolveOwn('../../dist'),
         appPublic: resolveOwn(`${templatePath}/public`),
         appHtml: resolveOwn(`${templatePath}/public/index.html`),
         appIndexJs: resolveModule(resolveOwn, `${templatePath}/src/index`),
