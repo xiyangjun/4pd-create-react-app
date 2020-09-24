787ba99ee24b863a616e3d6eb6be3e8a0dfbfe2a
zhao jun <zhaojun@zhaojundeMacBook-Pro-3.local>
2020-09-24 12:24:24 +0800

    bugfix:d.ts指向4pd-react-scripts

--- packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
+++ packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
@@ -300,7 +300,7 @@ function verifyTypeScriptSetup() {
     if (!fs.existsSync(paths.appTypeDeclarations)) {
         fs.writeFileSync(
             paths.appTypeDeclarations,
-            `/// <reference types="react-scripts" />${os.EOL}`
+            `/// <reference types="4pd-react-scripts" />${os.EOL}`
         );
     }
 }
